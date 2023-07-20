dad737bd24b58ad1dd073a4cff183801088f0d70
dilshodbekkhamroev98@gmail.com <dilshodbekkhamroev98@gmail.com>
2023-07-20 13:47:02 +0500

    Add Program File

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Guru
+{
+class Program
+{
+static void Main()
+{
+    System.Console.Write("Enter the radius: ");
+    double radius = Convert.ToDouble(Console.ReadLine());
+    const float pi = 3.14f;
+    System.Console.WriteLine($"Space: {pi}*({radius}*{radius})="+ pi*radius*radius);
+}
+}
+}
