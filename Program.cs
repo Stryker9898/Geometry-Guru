8e3ce841c7f78f06d43be9e32cdca08cb57e0b57
dilshodbekkhamroev98@gmail.com <dilshodbekkhamroev98@gmail.com>
2023-07-25 14:03:38 +0500

    Add Switch

--- Program.cs
+++ Program.cs
@@ -1,14 +1,40 @@
 ﻿using System;
 namespace Guru
 {
-class Program
-{
-static void Main()
-{
-    System.Console.Write("Enter the radius: ");
-    double radius = Convert.ToDouble(Console.ReadLine());
-    const float pi = 3.14f;
-    System.Console.WriteLine($"Space: {pi}*({radius}*{radius})="+ pi*radius*radius);
-}
-}
+    class Program
+    {
+        static void Main()
+        {
+            System.Console.WriteLine("Choose the shape");
+            System.Console.WriteLine("1.Circle");
+            System.Console.WriteLine("2.Triangle");
+            System.Console.WriteLine("3.1Rhombus");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+            switch (number)
+            {
+                case 1:
+                    {
+                        System.Console.Write("Enter the radius: ");
+                        double radius = Convert.ToDouble(Console.ReadLine());
+                        const float pi = 3.14f;
+                        System.Console.WriteLine($"Space: {pi}*({radius}*{radius})=" + pi * radius * radius);
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Enter the height and side of the triangle");
+                        Console.Write("Height:");
+                        double heightOfTriangle = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("Side:");
+                        double sideOfTriangle = Convert.ToDouble(Console.ReadLine());
+                        
+                        double areaOfTriagle = sideOfTriangle * heightOfTriangle/2;
+                        Console.WriteLine($"Area of a triangle : {sideOfTriangle}*{heightOfTriangle}/2="+areaOfTriagle);
+                        break;
+                    }
+            }
+
+        }
+    }
 }
